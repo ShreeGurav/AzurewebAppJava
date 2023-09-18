b2589eb8c662504b585965d2777ad55a672ea66e
ShrishailHugar <shrishailhugar@test.com>
2023-09-18 19:19:43 +0530

    Initial commit

---
+++ src/main/java/com/example/AzurewebAppJava/AzurewebAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.AzurewebAppJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurewebAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurewebAppJavaApplication.class, args);
+	}
+
+}
